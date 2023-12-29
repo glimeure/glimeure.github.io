4219346590372b86a824985c8f9ed30be15420eb
Gregory <49094805+glimeure@users.noreply.github.com>
2023-12-29 11:49:02 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const swiper = new Swiper("swiper-container");
+
+function checkViewportWidth() {
+	var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+	var wrappers = document.querySelectorAll("wrapper");
+	var viewportAlerts = document.querySelectorAll("viewport-alert");
+	if (viewportWidth < 425) {
+		wrappers.forEach((wrapper) => {
+			wrapper.style.display = "none";
+		});
+		viewportAlerts.forEach((viewportAlert) => {
+			viewportAlert.style.display = "flex";
+		});
+	} else {
+		wrappers.forEach((wrapper) => {
+			wrapper.style.display = "";
+		});
+		viewportAlerts.forEach((viewportAlert) => {
+			viewportAlert.style.display = "none";
+		});
+	}
+}
+
+checkViewportWidth();
+window.addEventListener("resize", function () {
+	checkViewportWidth();
+});
