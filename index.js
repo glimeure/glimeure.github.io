a2dada8e6364ee08c78d96733c9afcbb65320ebb
Gregory <49094805+glimeure@users.noreply.github.com>
2024-01-03 17:07:16 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function checkViewportWidth() {
 	var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 	var wrappers = document.querySelectorAll("wrapper");
 	var viewportAlerts = document.querySelectorAll("viewport-alert");
-	if (viewportWidth < 280) {
+	if (viewportWidth < 360) {
 		wrappers.forEach((wrapper) => {
 			wrapper.style.display = "none";
 		});
